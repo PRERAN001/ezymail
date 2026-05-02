322f17f5af2eadef7ffb31cd122316ebfbc3678e
prerans <preran248@gmail.com>
2026-05-02 10:44:26 +0530

    version 1.1 of ezy mail

--- test.js
+++ test.js
@@ -1,7 +1,19 @@
-import ezymail from "ezymail";
+const ezymail = require("ezymail");
+
 ezymail.send({
-    from:"preran248@gmail.com",
-    to:"preran866@gmail.com",
-    subject:"test mail lol",
-    body:"This is a test mail sent using ezymail library"
-})
+  from: "preran248@gmail.com",
+  to: "preran866@gmail.com",
+  subject: "test mail lol",
+  body: `<!DOCTYPE html>
+<html>
+  <body style="font-family: Arial; background:#f4f4f4; padding:20px;">
+    <h1 style="color:#4f46e5;">🚀 EzyMail</h1>
+    <p>This is a HTML email.</p>
+
+    <a href="https://example.com"
+       style="background:#4f46e5; color:white; padding:10px 15px; text-decoration:none;">
+       Visit Website
+    </a>
+  </body>
+</html>`
+});
