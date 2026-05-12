e7d9dcb39166fa1804e22da0a5d39de3210928d2
prerans <preran248@gmail.com>
2026-05-12 21:58:27 +0530

    back to v1

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-async function send(data) {
-   const htmlContent = data?.html ?? data?.body;
-
-   if (!htmlContent) {
-      throw new Error("Missing content: provide html or body");
-   }
-
-   const payload = {
-      ...data,
-      html: htmlContent,
-      body: data?.body ?? htmlContent
-   };
-
-   const res = await fetch(
-      "https://traditions-described-mark-swing.trycloudflare.com/send",
-      {
-         method: "POST",
-         headers: {
-            "Content-Type": "application/json"
-         },
-         body: JSON.stringify(payload)
-      }
-   );
-
-   return res.json();
-}
-
-module.exports = { send };
+const { sendMail } = require("./lib/mailer");
+module.exports = {
+  send: sendMail
+};
