3317fc1e9a0b5ebfc63a8f795ffdbd2b9334c764
PRERAN S <prerans181@gmail.com>
2026-05-16 20:09:39 +0530

    Update API version in response message

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use((err, req, res, next) => {
 });
 
 app.get("/",(req,res)=>{
-   res.send("EzyMail API is running");
+   res.send("EzyMail API is running v 2.0.5");
 })
 app.post("/send", async (req, res) => {
    try {
